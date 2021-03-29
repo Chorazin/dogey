68bfb8fc1cb2b36990bc9222ba185022a0d32e57
Chorazin <chorazin@gmail.com>
2021-03-29 15:06:48 +1100

    css transitions on hovers for menu items

--- header.php
+++ header.php
@@ -10,9 +10,10 @@
 <head>
 	<title>Dogey's</title>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport"
-	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!-- cutom styles directory -->
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" type="text/css" media="screen" />
 	<?php wp_head(); ?>
 </head>
 <body>
@@ -24,7 +25,7 @@ if ( function_exists( 'wp_body_open' ) ) {
 }
 ?>
 <div> <!-- content wrapper for main nav -->
-	<nav>
+	<nav class='main_nav bg-black bg-opacity-80 text-white'>
 		<?php
 			//set up nav menu, see functions.php for details
 			wp_nav_menu(
@@ -32,14 +33,14 @@ if ( function_exists( 'wp_body_open' ) ) {
 					'menu' => 'main_nav_links',
 					'container' => '',
 					'theme_location' => 'primary_menu',
-					'items_wrap' => '<ul id="" class="font-bold flex items-stretch main_nav">%3$s</ul>'
+					'items_wrap' => '<ul id="" class="font-bold flex items-stretch p-4">%3$s</ul>'
 				)
 			);
 		?>
 	</nav>
 </div>
 	<header>
-		<div>
+		<div class='hero'>
 
 		</div>
 		Header content here
