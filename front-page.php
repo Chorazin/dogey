b7839ef388b8e8b8fea730ae1bcb3cde207ad478
Chorazin <chorazin@gmail.com>
2021-04-07 15:25:14 +1000

    final push of the day yay

--- front-page.php
+++ front-page.php
@@ -11,13 +11,11 @@
      <?php /* $my_query = new WP_Query(array('post_type' => 'post','orderby' => 'date' ,'order' => 'DESC','ignore_sticky_posts' => false 'posts_per_page' => 3)); */ ?>
      <?php $my_query = new WP_Query(array('post__in'=>get_option('sticky_posts')));
      while ($my_query->have_posts()) : $my_query->the_post(); ?>
-
      <div>
-        	<h3><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h3>
-          <h4><?php the_time('F jS, Y'); ?> <b>sticky</b><h4>
-    			<p><?php the_excerpt(); ?></p>
+      <h3><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h3>
+      <h4><?php the_time('F jS, Y'); ?> <b>sticky</b><h4>
+      <p><?php the_excerpt(); ?></p>
      </div>
-
    <?php endwhile; ?>
    <!-- setup a counter for number of posts to be displayed minus sticky posts currently displayed -->
    <?php
@@ -29,16 +27,15 @@
    }
    ?>
   <!---------------------------------------------------------------------------------------------------->
-
   <!-- now fetch me all the posts excluding sticky posts which have already been displayed -->
   <!---------------------------------------------------------------------------------------------------->
   <!-- THE LOOP! -->
-  <?php $my_query_2 = new WP_Query(array( 'post__not_in' => get_option( 'sticky_posts' ), 'posts_per_page'=> $final_count)  );
+  <?php $my_query_2 = new WP_Query(array( 'post__not_in' => get_option( 'sticky_posts' ), 'posts_per_page'=> $final_count));
   while ($my_query_2->have_posts()) : $my_query_2->the_post(); ?>
   <div>
-       <h3><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h3>
-       <h4><?php the_time('F jS, Y'); ?> <b></b><h4>
-       <p><?php the_excerpt(); ?></p>
+    <h3><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h3>
+    <h4><?php the_time('F jS, Y'); ?> <b></b><h4>
+    <p><?php the_excerpt(); ?></p>
   </div>
   <?php endwhile; ?>
   <!---------------------------------------------------------------------------------------------------->
