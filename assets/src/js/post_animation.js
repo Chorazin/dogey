7205287c3aa8b81100edbaf5350589571dcc9054
Chorazin <chorazin@gmail.com>
2021-04-14 14:52:52 +1000

    animating hr's and images

--- assets/src/js/post_animation.js
+++ assets/src/js/post_animation.js
@@ -1,15 +1,20 @@
 //fetch the nodelists required
 let post_cards = document.querySelectorAll('div.card');
 let book_icons = document.querySelectorAll('img.b_icon');
+let dividers = document.querySelectorAll('hr');
 
+console.log(dividers);
 //setup foreach to iterate through post_cards taking in an index for the second nodelist book_icons
 post_cards.forEach((card, index) => {
   //set icon to the current index of the forEach loop
   const icon = book_icons[index]
+  const divider = dividers[index]
 
   card.addEventListener('mouseover', () => {
     card.classList.add('animate_card');
     card.classList.remove('animate_card_out');
+    divider.classList.add('animate_hr');
+    divider.classList.remove('animate_hr_out');
     icon.classList.add('b_icon_animate');
     icon.classList.remove('b_icon_animate_out');
     icon.src = `${template_url}/assets/src/imgs/book_v003.png`;
@@ -18,6 +23,8 @@ post_cards.forEach((card, index) => {
   card.addEventListener('mouseleave', () => {
     card.classList.add('animate_card_out');
     card.classList.remove('animate_card');
+    divider.classList.remove('animate_hr');
+    divider.classList.add('animate_hr_out');
     icon.classList.remove('b_icon_animate');
     icon.classList.add('b_icon_animate_out');
     icon.src = `${template_url}/assets/src/imgs/book_v002.png`;
