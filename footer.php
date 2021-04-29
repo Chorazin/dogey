90f2658e41d705dbe25064bf576d9c7b48fe1715
Chorazin <chorazin@gmail.com>
2021-04-29 14:45:02 +1000

    commence footer .... engage!

--- footer.php
+++ footer.php
@@ -7,8 +7,35 @@
 ?>
 
 
-<footer>
-	Footer content here please
+<footer class='footer_background_image text-white relative'>
+	<div class='grid lg:grid-cols-4 gap-x-8 pt-20 pl-20'>
+		<div>
+			<h1 class='text-xl font-bold mb-5'>STAY CONNECTED</h1>
+			<p>Join us on social media for sweet deals and promotions!</p>
+			<div class='mt-5 flex flex-row justify-between'>
+				<div><img src="<?php bloginfo('template_url'); ?>/assets/src/imgs/fb_v001.png" alt="Facebook Icon"></div>
+				<div><img src="<?php bloginfo('template_url'); ?>/assets/src/imgs/insta_v001.png" alt="Instagram Icon"></div>
+				<div><img src="<?php bloginfo('template_url'); ?>/assets/src/imgs/tt_v001.png" alt="TikTok Icon"></div>
+			</div>
+		</div>
+
+		<div>
+			<h1 class='text-xl font-bold mb-5'>COMMITTED TO EXCELLENCE</h1>
+			<p>It's not just about getting the service done, it's about the attention to detail and consistency of the Mobile Groom Room. Trust us with your fur babies, they deserve the best!</p>
+		</div>
+
+		<div>
+			<h1 class='text-xl font-bold mb-5'>NAVIGATE</h1>
+		</div>
+		<div>
+			<h1 class='text-xl font-bold mb-5'>LOCATION</h1>
+		</div>
+	</div>
+	<div class="container inset_center">
+		<hr />
+		<div>COPYRIGHT LALALALA</div>
+		<hr/>
+	</div>
 </footer>
 <?php wp_footer(); ?>
 <script src="<?php bloginfo('template_url'); ?>/assets/src/js/navbar.js"></script>
