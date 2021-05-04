0f3c75a6f6cd997d7c6ca2b6a39b98685e510bfb
Chorazin <chorazin@gmail.com>
2021-05-04 13:40:29 +1000

    fixinf WP_Query of 0

--- front-page.php
+++ front-page.php
@@ -11,27 +11,31 @@
    <!-- tailwind cards go in here for each post -->
    <div class='grid lg:grid-cols-3 gap-x-8 pt-5 pb-5 ml-5 mr-5 posts text-white'>
      <!-- fetch all sticky posts first -->
-     <?php $my_query = new WP_Query(array('post__in'=>get_option('sticky_posts')));
-     while ($my_query->have_posts()) : $my_query->the_post(); ?>
+     <?php
+      if(count(get_option('sticky_posts')) > 0){
+        $my_query = new WP_Query(array('post__in'=>get_option('sticky_posts')));
+        while ($my_query->have_posts()) : $my_query->the_post(); { ?>
 
-     <div class='rounded-md content-center shadow-lg mb-8 card pr-5 pl-5'>
-       <div class='inner_card text-left flex justify-between'>
-         <span class='font-bold text-2xl'><?php the_title(); ?></span>
-         <span><img src="<?php bloginfo('template_url'); ?>/assets/src/imgs/book_v002.png" alt="Open book Icon" class='b_icon mt-3'></span>
-       </div>
-       <span class='text-xs block text-left'><?php the_time('F jS, Y'); ?> <b>sticky</b></span>
-       <hr />
-       <div class='mt-10 mb-10 pb-10'><?php the_excerpt(); ?></div>
-       <div class='post_paws'>
-         <span><img src="<?php bloginfo('template_url'); ?>/assets/src/imgs/paw_v001.png" alt="tiny dogs paw" class='scale_rotate_paw'></span>
-       </div>
-     </div>
-     <script type="text/javascript">
-       count_j++
-       add_click('<?php echo get_permalink(); ?>', count_j)
-      </script>
-   <?php endwhile; ?>
-   <!-- setup a counter for number of posts to be displayed minus sticky posts currently displayed -->
+      <div class='rounded-md content-center shadow-lg mb-8 card pr-5 pl-5'>
+        <div class='inner_card text-left flex justify-between'>
+          <span class='font-bold text-2xl'><?php the_title(); ?></span>
+          <span><img src="<?php bloginfo('template_url'); ?>/assets/src/imgs/book_v002.png" alt="Open book Icon" class='b_icon mt-3'></span>
+        </div>
+        <span class='text-xs block text-left'><?php the_time('F jS, Y'); ?> <b>sticky</b></span>
+        <hr />
+        <div class='mt-10 mb-10 pb-10'><?php the_excerpt(); ?></div>
+        <div class='post_paws'>
+          <span><img src="<?php bloginfo('template_url'); ?>/assets/src/imgs/paw_v001.png" alt="tiny dogs paw" class='scale_rotate_paw'></span>
+        </div>
+      </div>
+      <script type="text/javascript">
+        count_j++
+        add_click('<?php echo get_permalink(); ?>', count_j)
+       </script>
+    <?php } endwhile;
+  }?>
+
+  <!-- setup a counter for number of posts to be displayed minus sticky posts currently displayed -->
    <?php
    $count = count(get_option('sticky_posts'));
    $final_count = 3 - $count;
@@ -44,7 +48,8 @@
   <!-- now fetch me all the posts excluding sticky posts which have already been displayed -->
   <!---------------------------------------------------------------------------------------------------->
   <!-- THE LOOP! -->
-  <?php $my_query_2 = new WP_Query(array( 'post__not_in' => get_option( 'sticky_posts' ), 'posts_per_page'=> $final_count));
+  <?php if($final_count > 0) {
+  $my_query_2 = new WP_Query(array( 'post__not_in' => get_option( 'sticky_posts' ), 'posts_per_page'=> $final_count));
   while ($my_query_2->have_posts()) : $my_query_2->the_post(); ?>
 
   <div class='rounded-md content-center shadow-lg mb-8 card pr-5 pl-5'>
@@ -63,7 +68,8 @@
     count_j++
     add_click('<?php echo get_permalink(); ?>', count_j)
    </script>
-  <?php endwhile; ?>
+  <?php endwhile;
+}?>
   <!---------------------------------------------------------------------------------------------------->
  </div>
 
